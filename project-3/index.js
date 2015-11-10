3f8e83774f7451922e73b2717ff0a8333bb250cd
kkreative <kkreative@outlook.com>
2015-11-10 08:48:28 -0500

    adding checkbox line through functionality when an item is checked

--- project-3/index.js
+++ project-3/index.js
@@ -1,12 +1,20 @@
 $(document).ready(function (){
     $(".add-items").submit(function(event) {
       event.preventDefault();
-      // console.log($("#todo-list-item").val());
 
       var item = $("#todo-list-item").val();
 
-      $("#list-items").append("<li><input type='checkbox'/>" + item + "<a class='remove'>x</a><hr /></li>");
-      $("#todo-list-item").val("");
-
+      if (item) {
+        $("#list-items").append("<li><input class='checkbox' type='checkbox'/>" + item + "<a class='remove'>x</a><hr /></li>");
+        $("#todo-list-item").val("");
+      }
    });
+
+  //  $(".checkbox").changed(function(){
+  //    console.log("Checkbox checked!");
+  //  })
+
+  $(document).on("change", ".checkbox", function(){
+    $(this).parent().toggleClass("completed");
+  })
 });
