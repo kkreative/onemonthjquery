58a5f1210ac69181290e0d6bddf44abb063ff9a7
kkreative <kkreative@outlook.com>
2015-11-14 22:36:59 -0500

    minor changes to the style.css and attempts at animating item add

--- project-3/index.js
+++ project-3/index.js
@@ -5,28 +5,27 @@ $(document).ready(function (){
       event.preventDefault();
 
       var item = $("#todo-list-item").val();
-
+// If text is entered into input, then append the input text as <li>
       if (item) {
         $("#list-items").append("<li><input class='checkbox' type='checkbox'/>" + item + "<a class='remove'>x</a><hr /></li>");
         localStorage.setItem("listItems", $("#list-items").html());
         $("#todo-list-item").val("");
       }
    });
-
-  //  $(".checkbox").changed(function(){
-  //    console.log("Checkbox checked!");
-  //  })
-
+ 
+  // onChange check/uncheck the boxes
   $(document).on("change", ".checkbox", function(){
     if ($(this).attr("checked")) {
          $(this).removeAttr("checked");
        } else {
          $(this).attr("checked", "checked");
        }
-
+  // onChange toggle the <li> class to strikethrough the text
     $(this).parent().toggleClass("completed");
     localStorage.setItem("listItems", $("#list-items").html());
   });
+
+  // onClick remove the <li> from the list
   $(document).on("click", ".remove", function(){
     $(this).parent().remove();
     localStorage.setItem("listItems", $("#list-items").html());
